788b172d0fe3daf79955f89af2a77964b8d63ff4
LennartHildebrandt <eneli317@htw-dresden.de>
2024-11-13 08:13:37 +0100

    updated query logic

--- src/main/java/datacat/graphql/GraphQLClass.java
+++ src/main/java/datacat/graphql/GraphQLClass.java
@@ -14,7 +14,9 @@ public class GraphQLClass {
     private static final Logger logger = LoggerFactory.getLogger(GraphQLClass.class);
 
     // =====================================================================================================================
-    // api/Class/v1
+    // SECTION: CLASS
+    // =====================================================================================================================
+    // ENDPOINT: /api/Class/v1
     // class
     // features that are not implemented yet are commented out
     // lesser effort: 
@@ -49,15 +51,16 @@ public class GraphQLClass {
                     .append("assignedCollections { nodes { parentClassReference:relatedCollections { uri:id name } } } ");
 
         if (includeProperties == true) {
-            queryBuilder.append(getClassPropertiesQuery());
+            queryBuilder.append(getClassPropertyQuery());
             logger.debug("Query includes properties");
         }
         queryBuilder.append("} }");
+
         return queryBuilder.toString();
     }
 
     // properties
-    public static String getClassPropertiesQuery() {
+    public static String getClassPropertyQuery() {
         StringBuilder queryBuilder = new StringBuilder();
         queryBuilder.append("classProperties:properties { ")
                     .append("name ")
@@ -70,6 +73,42 @@ public class GraphQLClass {
                     .append("composedBy { nodes { propertySet:name } } ")
                     .append("assignedMeasures { nodes { relatedMeasures { assignedUnits { nodes { units:relatedUnits { name } } } } } } ")
                     .append("}");
+
+        return queryBuilder.toString();
+    }
+
+    // =====================================================================================================================
+    // ENDPOINT: /api/Class/Relations/v1
+
+
+    // =====================================================================================================================
+    // ENDPOINT: /api/Class/Properties/v1
+    public static String getClassPropertiesQuery(String id, int queryOffset, int queryLimit, String languageCode) {
+        StringBuilder queryBuilder = new StringBuilder();
+        // queryBuilder.append("{ findSubjects(input:{pageSize: \\\"").append(queryLimit).append("\\\" idIn: \\\"").append(id).append("\\\"} ) { ")
+        queryBuilder.append("{ findSubjects(input:{pageSize: ").append(queryLimit).append(" idIn: \\\"").append(id).append("\\\"} ) { ")
+                    .append("pageInfo { count:pageElements } ")
+                    .append("totalCount:totalElements ")
+
+                    .append("nodes { ")
+                    .append("classUri:id ")
+
+                    .append("classProperties:properties { ")
+                    .append("name ")
+                    .append("description ")
+                    .append("uid:id ")
+                    .append("uri:id ")
+                    .append("propertyCode:name ")
+                    .append("assignedMeasures { nodes { relatedMeasures { assignedUnits { nodes { allowedValues:relatedUnits { uri:id value:name description } } } } } } ")
+                    // .append("documentedBy { nodes { relatingDocument { propertyDictionaryName:name propertyDictionaryUri:id } } } ")
+                    .append("composedBy { nodes { propertySet:name } } ")
+                    .append("assignedMeasures { nodes { relatedMeasures { assignedUnits { nodes { units:relatedUnits { name } } } } } } ")
+                    .append("}")
+
+                    .append("}")
+
+                    .append("} }");
+
         return queryBuilder.toString();
     }
 }
--- src/main/java/datacat/graphql/GraphQLDictionary.java
+++ src/main/java/datacat/graphql/GraphQLDictionary.java
@@ -14,11 +14,11 @@ public class GraphQLDictionary {
     // =====================================================================================================================
     // ENDPOINT: /api/Dictionary/v1
     // OPTION 1: query to fetch all dictionaries
-    public static String getDictionary(String id, String bearerToken) {
+    public static String getOneDictionary(String id, String bearerToken) {
         final String dictTag = "6f96aaa7-e08f-49bb-ac63-93061d4c5db2"; // 'dictTag' is a constant internal value pointing only on models in XtdBag
         StringBuilder queryBuilder = new StringBuilder();
         queryBuilder.append("{ findBags(input:{pageSize:1 tagged:\\\"" + dictTag + "\\\" idIn: \\\"" + id + "\\\"}) { ")
-                    .append("dictionaries:nodes { ")
+                    .append("nodes { ")
                     .append("uri:id ")
                     .append("name ")
                     .append("version:versionId ")
@@ -33,7 +33,7 @@ public class GraphQLDictionary {
         final String dictTag = "6f96aaa7-e08f-49bb-ac63-93061d4c5db2"; // 'dictTag' is a constant internal value pointing only on models in XtdBag
         StringBuilder queryBuilder = new StringBuilder();
         queryBuilder.append("{ findBags(input:{pageSize:10000 tagged: \\\"" + dictTag + "\\\"}) { ")
-                    .append("dictionaries:nodes { ")
+                    .append("nodes { ")
                     .append("uri:id ")
                     .append("name ")
                     .append("version:versionId ")
@@ -48,7 +48,7 @@ public class GraphQLDictionary {
     // STEP 1: first query to fetch the classes related to the requested dictionary
     public static String getDictionaryGroupQuery(String id, String languageCode) {
         StringBuilder queryBuilder = new StringBuilder();
-        queryBuilder.append("{getBag(id:\\\"" + id + "\\\") { ")
+        queryBuilder.append("{ getBag(id:\\\"" + id + "\\\") { ")
                     .append("name ")
                     .append("uri:id ")
                     .append("code:name ")
@@ -62,7 +62,7 @@ public class GraphQLDictionary {
     // // STEP 2: queries fetch classes related to certain groups
     public static String getGroupClassesQuery(String groupId, int limit, String languageCode) {
         StringBuilder queryBuilder = new StringBuilder();
-        queryBuilder.append("{getBag(id:\\\"" + groupId + "\\\") { ")
+        queryBuilder.append("{ getBag(id:\\\"" + groupId + "\\\") { ")
                     // .append("collects { nodes { relatedThings { uri:id name descriptionPart:description } } }")
                     // .append("collects { nodes { relatedThings { name } } }")
                     .append("collects { ")
