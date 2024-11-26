81db159b13311fec082b9fd75e053b0acb8c8ed7
LennartHildebrandt <eneli317@htw-dresden.de>
2024-11-26 12:24:13 +0100

    auto spring doc update

--- src/main/java/org/openapitools/configuration/SpringDocConfiguration.java
+++ src/main/java/org/openapitools/configuration/SpringDocConfiguration.java
@@ -1,38 +1,60 @@
-// package org.openapitools.configuration;
+package org.openapitools.configuration;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.info.Info;
-// import io.swagger.v3.oas.models.info.Contact;
-// import io.swagger.v3.oas.models.info.License;
-// import io.swagger.v3.oas.models.Components;
-// import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.List;
 
-// @Configuration
-// public class SpringDocConfiguration {
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.tags.Tag;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
-//     @Bean(name = "org.openapitools.configuration.SpringDocConfiguration.apiInfo")
-//     OpenAPI apiInfo() {
-//         return new OpenAPI()
-//                 .info(
-//                         new Info()
-//                                 .title("Datacat REST API v1.0.5")
-//                                 .description("<p>Web facade to access the Datacat catalogue system.</br></p> <h3>Latest changes:</h3> <ul><li>v1.0.X \"major endpoint overhaul\"<ul>     <li>general rework of api/Class/v1</li>     <li>general rework of api/Dictionary/v1/Classes (upcoming)</li>     <li>including filter options (missing business logic)</li>     <li>schema adjustment (including non-implemented attributes)</li>     <li>backend logic and query rework</li>     <li>minor UI adjustments</li>     <li>improved accessibility of specification</li> </ul><li>v1.0.0 \"bSDD-like prototype\"<ul>     <li>adjustment of existing endpoints to match bSDD</li>     <li>including URI usage (partly missing business logic)</li>     <li>schema rework (renaming, parameter changes, etc.)</li>     <li>removed porxy settings</li>     <li>published under CAFM and IBPDI demain</li>     <li>minor changes to the UI</li> </ul></ul></li></ul>")
-//                                 .contact(
-//                                         new Contact()
-//                                                 .name("Support/Contact")
-//                                                 .url("https://github.com/dd-bim")
-//                                                 .email("lennart.hildebrandt@htw-dresden.de")
-//                                 )
-//                                 .license(
-//                                         new License()
-//                                                 .name("* licence name placeholder *")
-//                                                 .url("http://unlicense.org")
-//                                 )
-//                                 .version("1.0.5")
-//                 )
-//         ;
-//     }
-// }
+@Configuration
+public class SpringDocConfiguration {
+
+    @Bean(name = "org.openapitools.configuration.SpringDocConfiguration.apiInfo")
+    OpenAPI apiInfo() {
+        return new OpenAPI()
+            .info(
+                new Info()
+                    .title("Datacat REST API")
+                    .description("<p>OpenAPI-based API to access the Datacat catalogue system.</br></p> <h3>Upcoming changes:</h3> <ul><li>v1.2.X \"major endpoint overhaul\"<ul>     <li>step-by-step inclusion of missing endpoints</li> </ul><li></ul> <h3>Latest changes:</h3> <ul><li>v1.2.1 \"major endpoint overhaul\"<ul>     <li>putting /api/Class/Properties/v1 under rework</li>     <li>putting /api/Dictionary/v1 under rework</li>     <li>minor improvements for v1.1</li>     <li>temporarily removed server drop down menu</li> </ul><li>v1.1.1 \"ekkodale minimal requirements\"<ul>     <li>general rework of /api/Class/v1</li>     <li>general rework of /api/Dictionary/v1/Classes</li>     <li>schema adjustment (including non-implemented attributes)</li>     <li>backend logic and query rework</li>     <li>minor UI adjustments</li>     <li>improved accessibility of specification</li> </ul></li></ul>")
+                    .contact(
+                        new Contact()
+                            .name("Support/Contact")
+                            .url("https://github.com/dd-bim")
+                            .email("lennart.hildebrandt@htw-dresden.de")
+                    )
+                    .license(
+                            new License()
+                                    .name("* licence name placeholder *")
+                                    .url("http://unlicense.org")
+                    )
+                    .version("1.2.2")
+    )
+    .tags(List.of(
+        new Tag().name("Class").description("Operations about classes"),
+        new Tag().name("Dictionary").description("Operations about dictionaries"),
+        new Tag().name("Popular Dictionary").description("Operations related to popular dictionaries"),
+        new Tag().name("Property").description("Operations about properties"),
+        new Tag().name("Search").description("Operations related to search"),
+        new Tag().name("Lookup Data").description("Operations related to lookup data"),
+        new Tag().name("Datacat Specifics").description("Datacat specific operations")
+    ));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+}
